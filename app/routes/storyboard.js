5fab94416a4c341437070db553e6225ca7c76b0c
Chester Law <chesterlaw@me.com>
2016-11-05 00:11:06 +0900

    Make movie-screen work

--- app/routes/storyboard.js
+++ app/routes/storyboard.js
@@ -26,22 +26,28 @@ export default Ember.Route.extend({
       $('.movie-screen').show();
       $('.movie-screen__img').show();
 
+      $('.movie-screen__img').css('z-index', '-1');
+      $('.movie-screen .movie-screen__img:visible:nth-child(1)').css('z-index', '1');
+
       var durations = [];
       this.get('store').peekAll('cut').forEach(function(item) {
-        durations.push(item.get('duration')*1000);
+        if (durations.length === 0) {
+          durations.push(item.get('duration')*1000);
+        } else {
+          durations.push(item.get('duration')*1000 + durations[durations.length-1]);
+        }
       });
 
-      setTimeout(function() {
-        $('.movie-screen .movie-screen__img:visible:nth-child(1)').hide();
-      }, durations[0])
+      function doTimeout(i) {
+        setTimeout(function() {
+          $(`.movie-screen .movie-screen__img:visible:nth-child(${i+1})`).hide();
+          $(`.movie-screen .movie-screen__img:visible:nth-child(${i+2})`).css('z-index', '1');
+        }, durations[i])
+      }
 
-      setTimeout(function() {
-        $('.movie-screen .movie-screen__img:visible:nth-child(2)').hide();
-      }, durations[0]+durations[1])
-
-      setTimeout(function() {
-        $('.movie-screen .movie-screen__img:visible:nth-child(3)').hide();
-      }, durations[0]+durations[1]+durations[2])
+      for (var i=0; i < durations.length; i += 1) {
+        doTimeout(i)
+      }
 
       // hide .movie-screen if esc is pressed
       $(document).keyup(function(e) {
