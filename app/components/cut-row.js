a0e9a4f054518383c3e7cbb1c1080a90b4f7bca4
Chester Law <chesterlaw@me.com>
2016-11-04 18:47:52 +0900

    Make cut-row more Ember-like

--- app/components/cut-row.js
+++ app/components/cut-row.js
@@ -2,10 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: 'cut-row',
+  prevX: 0,
+  currX: 0,
+  prevY: 0,
+  currY: 0,
+  aspectRatio: 1.33333333,
+  flag: false,
+  dot_flag: false,
+  drawColor: 'black',
 
   cutNumber: Ember.computed('index', function() {
     return this.get('index') + 1;
   }),
+
+  draw() {
+    this.get('canvasContext').beginPath();
+    this.get('canvasContext').moveTo(this.get('prevX'), this.get('prevY'));
+    this.get('canvasContext').lineTo(this.get('currX'), this.get('currY'));
+    this.get('canvasContext').strokeStyle = this.get('drawColor');
+    this.get('canvasContext').stroke();
+    this.get('canvasContext').closePath();
+  },
 
   actions: {
     updateCutDuration() {
@@ -16,24 +33,17 @@ export default Ember.Component.extend({
     clearCanvas() {
       if (confirm('Really want to clear?')) {
         this.get('canvasContext').clearRect(0, 0, this.get('w'), this.get('h'));
-        // document.getElementById('canvasimg').style.display = 'none';
       }
     }
   },
 
   didInsertElement() {
-    var aspectRatio = 1.33333333;
-    var flag = false;
-    var prevX = 0;
-    var currX = 0;
-    var prevY = 0;
-    var currY = 0;
-    var dot_flag = false;
-    var drawColor = 'black';
+    var self = this;
     var canvas = this.element.querySelector('canvas');
+    this.set('canvas', canvas);
 
     var offsetHeight = canvas.offsetHeight;
-    var offsetWidth = offsetHeight*aspectRatio;
+    var offsetWidth = offsetHeight*this.get('aspectRatio');
     canvas.height = offsetHeight;
     canvas.width = offsetWidth;
 
@@ -45,71 +55,56 @@ export default Ember.Component.extend({
 
     canvasContext.lineWidth = 2;
 
-    canvas.addEventListener('mousemove', function (e) {
+    this.get('canvas').addEventListener('mousemove', function (e) {
       findxy('move', e)
     }, false);
-    canvas.addEventListener('mousedown', function (e) {
+    this.get('canvas').addEventListener('mousedown', function (e) {
       findxy('down', e)
     }, false);
-    canvas.addEventListener('mouseup', function (e) {
+    this.get('canvas').addEventListener('mouseup', function (e) {
       findxy('up', e)
     }, false);
-    canvas.addEventListener('mouseout', function (e) {
+    this.get('canvas').addEventListener('mouseout', function (e) {
       findxy('out', e)
     }, false);
 
     function enterEraseMode() {
-      drawColor = 'white';
+      self.set('drawColor', 'white');
     }
 
     function enterDrawMode(obj) {
-      drawColor = 'black';
-    }
-
-    function draw() {
-      canvasContext.beginPath();
-      canvasContext.moveTo(prevX, prevY);
-      canvasContext.lineTo(currX, currY);
-      canvasContext.strokeStyle = drawColor;
-      canvasContext.stroke();
-      canvasContext.closePath();
-    }
-
-    function save() {
-      var dataURL = canvas.toDataURL();
-      document.getElementById('canvasimg').src = dataURL;
-      document.getElementById('canvasimg').style.display = 'inline';
+      self.set('drawColor', 'black');
     }
 
     function findxy(res, e) {
       var boundingBox = canvas.getBoundingClientRect();
 
       if (res == 'down') {
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX - boundingBox.left;
-        currY = e.clientY - boundingBox.top;
+        self.set('prevX', self.get('currX'));
+        self.set('prevY', self.get('currY'));
+        self.set('currX', e.clientX - boundingBox.left);
+        self.set('currY', e.clientY - boundingBox.top);
 
-        flag = true;
-        dot_flag = true;
-        if (dot_flag) {
+        self.set('flag', true);
+        self.set('dot_flag', true);
+        if (self.get('dot_flag')) {
           canvasContext.beginPath();
-          canvasContext.fillStyle = drawColor;
-          canvasContext.fillRect(currX, currY, 2, 2);
+          canvasContext.fillStyle = self.get('drawColor');
+          canvasContext.fillRect(self.get('currX'), self.get('currY'), 2, 2);
           canvasContext.closePath();
-          dot_flag = false;
+          self.set('dot_flag', false);
         }
       }
       if (res == 'up' || res == 'out') {
-        flag = false;
+        self.set('flag', false);
       }
       if (res == 'move') {
-        if (flag) {
-          prevX = currX;
-          prevY = currY;
-          currX = e.clientX - boundingBox.left;
-          currY = e.clientY - boundingBox.top;
-          draw();
+        if (self.get('flag')) {
+          self.set('prevX', self.get('currX'));
+          self.set('prevY', self.get('currY'));
+          self.set('currX', e.clientX - boundingBox.left);
+          self.set('currY', e.clientY - boundingBox.top);
+          self.draw();
         }
       }
     }
