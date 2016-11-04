4090e5b2d30d1f27a4535d2c94cbaf7d4df8b3a3
Chester Law <chesterlaw@me.com>
2016-11-04 14:12:30 +0900

    Show all cuts as cut-rows

--- app/routes/storyboard.js
+++ app/routes/storyboard.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    this.get('store').createRecord('cut');
+    this.get('store').createRecord('cut');
+    this.get('store').createRecord('cut');
+    return this.get('store').peekAll('cut');
+  }
 });
